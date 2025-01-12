fd2498096616b107b929d2c94cbfd51f4396f807
ttarika <tushar.tarika1@gmail.com>
2025-01-12 12:25:42 +0530

    added loss calculation

--- LinearRegression_sample.py
+++ LinearRegression_sample.py
@@ -1,8 +1,14 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression
-
+from sklearn.metrics import mean_squared_error
 ### try to calculate coffecient and interpretor with example and data frame
+
+
+#df = pd.DataFrame({'x':[0,1,2,3,5,],'y':[ 1, 3, 5 , 7 , 11]})
+
+
 
 # 0.99, 3.02, 9.0 , 18.98 , 33.01
 df = pd.DataFrame({'x':[0,1,2,3,5,],'y':[ 0.99, 3.02, 9.0 , 18.98 , 33.01]})
@@ -18,3 +24,5 @@ w0 = model.intercept_;
 
 y_predict = model.predict(x);
 print(y_predict);
+
+print(round(np.sqrt(mean_squared_error(y, y_predict)), 0))
