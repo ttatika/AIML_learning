7e45050881b77b82a42d999a904ea88c53da2b3e
ttarika <tushar.tarika1@gmail.com>
2025-01-14 19:05:49 +0530

    plotting of KNN circles

--- KNN_plot_neigbour.py
+++ KNN_plot_neigbour.py
@@ -17,7 +17,7 @@ y = df['species'].values
 assert X.shape[0] == y.shape[0], "X and y must have the same number of samples"
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Initialize and train the KNN model
 model = KNeighborsClassifier(n_neighbors=3)
